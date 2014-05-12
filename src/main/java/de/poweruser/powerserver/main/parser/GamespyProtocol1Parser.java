917f2a3b9ba41f34a7c852b8e7bf2b98486d02a7
Poweruser <poweruser.rs@hotmail.com>
2014-05-12 14:14:13 +0200

    Checking the number of parts after splitting is not reliable, when receiving multiple messages concatenated into one. Skipping empty key parts works better.

--- src/main/java/de/poweruser/powerserver/main/parser/GamespyProtocol1Parser.java
+++ src/main/java/de/poweruser/powerserver/main/parser/GamespyProtocol1Parser.java
@@ -19,10 +19,13 @@ public class GamespyProtocol1Parser implements DataParserInterface {
         if(start < 0) { throw new ParserException("The message does not contain a backslash \"\\\". Is it a gamespy protocol version 2 message?", message, game); }
         message = message.substring(start + 1);
         String[] split = message.split("\\\\");
-        if((split.length % 2) != 0) { throw new ParserException("The number of elements in a gamespy protocol version 1 message must be even.", message, game); }
         HashMap<String, String> map = new HashMap<String, String>();
         for(int i = 0; i < split.length - 1; i = i + 2) {
             String key = split[i];
+            if(key.trim().isEmpty()) {
+                i--;
+                continue;
+            }
             String value = split[i + 1];
             if(game == null || game.verifyDataKeyAndValue(key, value)) {
                 map.put(key, value);
