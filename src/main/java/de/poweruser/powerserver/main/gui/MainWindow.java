cbe939d0478aa9acba58036bd4cc5da45ba531ad
Poweruser <poweruser.rs@hotmail.com>
2014-05-20 02:28:12 +0200

    Built a little gui, but it doesnt show anything yet

--- src/main/java/de/poweruser/powerserver/main/gui/MainWindow.java
+++ src/main/java/de/poweruser/powerserver/main/gui/MainWindow.java
@@ -1,10 +1,17 @@
 package de.poweruser.powerserver.main.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.WindowEvent;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 
 import de.poweruser.powerserver.main.PowerServer;
 
@@ -13,18 +20,38 @@ public class MainWindow extends JFrame implements Observer {
     private static final long serialVersionUID = 2846198182943968671L;
 
     private PowerServer server;
+    private boolean alreadyShuttingDown;
 
     public MainWindow(PowerServer server) {
         this.server = server;
+        this.alreadyShuttingDown = false;
         this.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
             public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-                if(JOptionPane.showConfirmDialog(MainWindow.this, "Do you really want to stop the master server?", "Really Closing?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
-                    MainWindow.this.server.shutdown();
+                if(!MainWindow.this.alreadyShuttingDown) {
+                    if(JOptionPane.showConfirmDialog(MainWindow.this, "Do you really want to stop the master server?", "Really Closing?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+                        MainWindow.this.server.shutdown();
+                        MainWindow.this.alreadyShuttingDown = true;
+                        MainWindow.this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+                        MainWindow.this.dispatchEvent(new WindowEvent(MainWindow.this, WindowEvent.WINDOW_CLOSING));
+                    }
                 }
             }
         });
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("PowerServer");
+        this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        JScrollPane pane = new JScrollPane();
+        pane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        pane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        JTextArea textArea = new JTextArea();
+        textArea.setEditable(true);
+        pane.add(textArea);
+        this.add(pane, BorderLayout.CENTER);
+        JTextField textField = new JTextField();
+        this.add(textField, BorderLayout.SOUTH);
+        this.setMinimumSize(new Dimension(500, 400));
+        this.setResizable(true);
         pack();
     }
 
