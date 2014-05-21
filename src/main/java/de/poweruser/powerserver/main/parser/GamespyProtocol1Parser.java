7b2d3a44d904eaf6a3cdbce2e60738cab431d652
Poweruser <poweruser.rs@hotmail.com>
2014-05-21 20:23:54 +0200

    When parsing was started without knowing what game it is for, then the parser will look for the gamename key. And once it is found, the parser will use that game, if it is valid, to verify all already parsed key-value pairs and all succeeding ones.

--- src/main/java/de/poweruser/powerserver/main/parser/GamespyProtocol1Parser.java
+++ src/main/java/de/poweruser/powerserver/main/parser/GamespyProtocol1Parser.java
@@ -2,6 +2,7 @@ package de.poweruser.powerserver.main.parser;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import de.poweruser.powerserver.games.GameBase;
 import de.poweruser.powerserver.games.GeneralDataKeysEnum;
@@ -10,6 +11,8 @@ import de.poweruser.powerserver.network.UDPMessage;
 
 public class GamespyProtocol1Parser implements DataParserInterface {
 
+    private GameBase parsedGame;
+
     @Override
     public MessageData parse(GameBase game, UDPMessage msg) throws ParserException {
         return this.parse(game, msg.toString());
@@ -17,6 +20,9 @@ public class GamespyProtocol1Parser implements DataParserInterface {
 
     @Override
     public MessageData parse(GameBase game, String message) throws ParserException {
+        if(game != null) {
+            this.parsedGame = game;
+        }
         int start = message.indexOf("\\");
         if(start < 0) { throw new ParserException("The message does not contain a backslash \"\\\". Is it a gamespy protocol version 2 message?", message, game); }
         message = message.substring(start + 1);
@@ -30,12 +36,12 @@ public class GamespyProtocol1Parser implements DataParserInterface {
                 continue;
             }
             String value = split[i + 1];
-            this.processPair(map, game, key, value, message);
+            this.processPair(map, key, value, message);
             if(this.isKeyFinalKey(key)) {
                 if(i + 3 < split.length) {
                     key = split[i + 2];
                     value = split[i + 3];
-                    this.processPair(map, game, key, value, message);
+                    this.processPair(map, key, value, message);
                 }
                 break;
             }
@@ -43,25 +49,40 @@ public class GamespyProtocol1Parser implements DataParserInterface {
         if(i == split.length - 1) {
             String key = split[i];
             if(this.isKeyFinalKey(key)) {
-                this.processPair(map, game, key, "", message);
+                this.processPair(map, key, "", message);
             }
         }
         return new MessageData(map);
     }
 
-    private void processPair(Map<String, String> map, GameBase game, String key, String value, String message) throws ParserException {
-        if(game == null || game.verifyDataKeyAndValue(key, value)) {
+    private void processPair(Map<String, String> map, String key, String value, String message) throws ParserException {
+        if(this.parsedGame == null && this.isGameNameKey(key)) {
+            GameBase foundGame = GameBase.getGameForGameName(value);
+            if(foundGame != null) {
+                this.parsedGame = foundGame;
+                for(Entry<String, String> entry: map.entrySet()) {
+                    if(!this.parsedGame.verifyDataKeyAndValue(entry.getKey(), entry.getValue())) { throw new ParserException("Found invalid data key \"" + key + "\" and value \"" + value + "\".", message, this.parsedGame); }
+                }
+            } else {
+                throw new ParserException("The gamename \"" + value + "\" in this message was not recognised.", message, this.parsedGame);
+            }
+        }
+        if(this.parsedGame == null || this.parsedGame.verifyDataKeyAndValue(key, value)) {
             map.put(key, value);
         } else {
-            throw new ParserException("Found invalid data key \"" + key + "\" and value \"" + value + "\".", message, game);
+            throw new ParserException("Found invalid data key \"" + key + "\" and value \"" + value + "\".", message, this.parsedGame);
         }
     }
 
     private boolean isKeyFinalKey(String key) {
-        return GeneralDataKeysEnum.FINAL.toString().equalsIgnoreCase(key);
+        return GeneralDataKeysEnum.FINAL.getKeyString().equalsIgnoreCase(key);
     }
 
     private boolean isKeyQueryKey(String key) {
-        return GeneralDataKeysEnum.QUERYID.toString().equalsIgnoreCase(key);
+        return GeneralDataKeysEnum.QUERYID.getKeyString().equalsIgnoreCase(key);
+    }
+
+    private boolean isGameNameKey(String key) {
+        return GeneralDataKeysEnum.GAMENAME.getKeyString().equalsIgnoreCase(key);
     }
 }
