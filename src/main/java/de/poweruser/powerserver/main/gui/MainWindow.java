d2315360da39dc88ac8c76554f3ac2f96eeac802
Poweruser <poweruser.rs@hotmail.com>
2014-05-24 08:02:06 +0200

    Added a command history and passing on of entered commands to the gui

--- src/main/java/de/poweruser/powerserver/main/gui/MainWindow.java
+++ src/main/java/de/poweruser/powerserver/main/gui/MainWindow.java
@@ -2,8 +2,11 @@ package de.poweruser.powerserver.main.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -23,10 +26,17 @@ public class MainWindow extends JFrame implements Observer {
 
     private PowerServer server;
     private boolean alreadyShuttingDown;
+    private ArrayList<String> commandHistory;
+    private CommandHistoryPos selectedCommandIndex;
+    private final int maxHistorySize;
 
     public MainWindow() {
         this.server = null;
         this.alreadyShuttingDown = false;
+        this.commandHistory = new ArrayList<String>();
+        this.selectedCommandIndex = CommandHistoryPos.POSITION;
+        this.maxHistorySize = 20;
+
         this.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
             public void windowClosing(java.awt.event.WindowEvent windowEvent) {
@@ -52,6 +62,42 @@ public class MainWindow extends JFrame implements Observer {
         textArea.setEditable(false);
         this.add(pane, BorderLayout.CENTER);
         JTextField textField = new JTextField();
+        textField.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyReleased(KeyEvent event) {}
+
+            @Override
+            public void keyPressed(KeyEvent event) {
+                if(event.getKeyCode() == KeyEvent.VK_ENTER) {
+                    JTextField source = (JTextField) event.getSource();
+                    String line = source.getText();
+                    if(!line.trim().isEmpty()) {
+                        source.setText("");
+                        MainWindow.this.addCommandToHistory(line);
+                        MainWindow.this.server.issueCommand(line);
+                    }
+                } else if(event.getKeyCode() == KeyEvent.VK_UP) {
+                    String previous = MainWindow.this.selectPreviousCommand();
+                    if(previous != null) {
+                        JTextField source = (JTextField) event.getSource();
+                        source.setText(previous);
+                    }
+                } else if(event.getKeyCode() == KeyEvent.VK_DOWN) {
+                    String next = MainWindow.this.selectNextCommand();
+                    if(next != null) {
+                        JTextField source = (JTextField) event.getSource();
+                        source.setText(next);
+                    }
+                } else if(event.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    JTextField source = (JTextField) event.getSource();
+                    source.setText("");
+                }
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {}
+        });
         this.add(textField, BorderLayout.SOUTH);
         this.setMinimumSize(new Dimension(600, 400));
         this.setResizable(true);
@@ -73,5 +119,71 @@ public class MainWindow extends JFrame implements Observer {
     public void setModel(PowerServer server) {
         this.server = server;
         this.server.addObserver(this);
+    }
+
+    private void addCommandToHistory(String line) {
+        if(!this.commandHistory.contains(line)) {
+            this.commandHistory.add(line);
+        }
+        while(this.commandHistory.size() > this.maxHistorySize) {
+            this.commandHistory.remove(0);
+        }
+        this.selectedCommandIndex.setValue(CommandHistoryPos.END);
+    }
+
+    private String selectPreviousCommand() {
+        if(this.selectedCommandIndex.equals(CommandHistoryPos.START)) { return null; }
+        if(this.selectedCommandIndex.equals(CommandHistoryPos.END) && this.commandHistory.size() > 0) {
+            this.selectedCommandIndex.setValue(this.commandHistory.size() - 1);
+        } else if(0 < this.selectedCommandIndex.getValue() && this.selectedCommandIndex.getValue() <= this.commandHistory.size()) {
+            this.selectedCommandIndex.setValue(this.selectedCommandIndex.getValue() - 1);
+        } else {
+            this.selectedCommandIndex.setValue(CommandHistoryPos.START);
+            return null;
+        }
+        return this.commandHistory.get(this.selectedCommandIndex.getValue());
+    }
+
+    private String selectNextCommand() {
+        if(this.selectedCommandIndex.equals(CommandHistoryPos.END)) { return null; }
+        if(this.selectedCommandIndex.equals(CommandHistoryPos.START) && this.commandHistory.size() > 0) {
+            this.selectedCommandIndex.setValue(0);
+        } else if(-1 <= this.selectedCommandIndex.getValue() && this.selectedCommandIndex.getValue() < this.commandHistory.size() - 1) {
+            this.selectedCommandIndex.setValue(this.selectedCommandIndex.getValue() + 1);
+        } else {
+            this.selectedCommandIndex.setValue(CommandHistoryPos.END);
+            return null;
+        }
+        return this.commandHistory.get(this.selectedCommandIndex.getValue());
+    }
+
+    private enum CommandHistoryPos {
+        START(Integer.MIN_VALUE),
+        POSITION(Integer.MAX_VALUE),
+        END(Integer.MAX_VALUE);
+
+        private int value;
+
+        private CommandHistoryPos(int value) {
+            this.value = value;
+        }
+
+        public boolean equals(CommandHistoryPos chp) {
+            return this.value == chp.value;
+        }
+
+        public void setValue(CommandHistoryPos chp) {
+            this.setValue(chp.value);
+        }
+
+        public void setValue(int value) {
+            if(this == POSITION) {
+                this.value = value;
+            }
+        }
+
+        public int getValue() {
+            return this.value;
+        }
     }
 }
