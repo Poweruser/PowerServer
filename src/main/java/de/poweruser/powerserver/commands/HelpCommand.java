1beca34b928379bd9820c8194b099f3baff11986
Poweruser <poweruser.rs@hotmail.com>
2014-05-25 04:34:14 +0200

    The help command should point out the commands command, otherwise it isnt of much help

--- src/main/java/de/poweruser/powerserver/commands/HelpCommand.java
+++ src/main/java/de/poweruser/powerserver/commands/HelpCommand.java
@@ -26,7 +26,8 @@ public class HelpCommand extends CommandBase {
         sb.append("Operation Flashpoint:Resistance  [opflashr]\n");
         sb.append("Arma: Cold War Assault  [opflashr]\n\n");
         sb.append("This application is open-source and the project's site is:  https://github.com/Poweruser/PowerServer/\n");
-        sb.append("Developed by Poweruser - 2014");
+        sb.append("Developed by Poweruser - 2014\n\n");
+        sb.append("Type 'commands' to see a list of available commands");
         Logger.logStatic(LogLevel.VERY_LOW, sb.toString());
 
     }
