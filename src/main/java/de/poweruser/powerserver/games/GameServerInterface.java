9f535250778b8e213c29e7badc06c119307c2d1a
Poweruser <poweruser.rs@hotmail.com>
2014-05-26 23:14:40 +0200

    More comments

--- src/main/java/de/poweruser/powerserver/games/GameServerInterface.java
+++ src/main/java/de/poweruser/powerserver/games/GameServerInterface.java
@@ -18,11 +18,12 @@ public interface GameServerInterface {
 
     /**
      * Checks whether the last heart-beat for this server has been received not
-     * longer ago, then the specified time duration.
+     * longer ago, than the specified time duration.
      * 
      * @param timeDiff
      *            The time duration to check
      * @param unit
+     *            The time unit of the provided time duration
      * @return true, if the last heart-beat was received within the specified
      *         duration, or if no heart-beat has been received yet
      *         false, if a heart-beat has been received already and it was
@@ -31,13 +32,66 @@ public interface GameServerInterface {
 
     public boolean checkLastHeartbeat(long timeDiff, TimeUnit unit);
 
+    /**
+     * Checks whether the last query reply of this server has been received not
+     * longer ago, than the specified time duration
+     * 
+     * @param timeDiff
+     *            The time duration to check
+     * @param unit
+     *            The time unit of the provided time duration
+     * @return true, if the last query reply was received within the specified
+     *         duration, or if no query reply has been received yet
+     *         false, if a query reply has been received already and it was
+     *         received longer ago than the specified duration
+     */
+
     public boolean checkLastQueryReply(long timeDiff, TimeUnit unit);
+
+    /**
+     * Checks whether the last query request of this server has been sent not
+     * longer ago, than the specified time duration
+     * 
+     * @param timeDiff
+     *            The time duration to check
+     * @param unit
+     *            The time unit of the provided time duration
+     * @return true, if the last query request was sent within the specified
+     *         duration, or if no query request has been sent yet
+     *         false, if a query request has been sent yet and it was sent
+     *         longer ago than the specified duration
+     */
 
     public boolean checkLastQueryRequest(long timeDiff, TimeUnit unit);
 
+    /**
+     * Checks if this server has answered to a query request yet
+     * 
+     * @return true, if a query answer has been received from this server yet,
+     *         otherwise false
+     */
     public boolean hasAnsweredToQuery();
 
+    /**
+     * Checks if another master server is sending heart-beat broadcasts
+     * for this game server, or if the master server is receiving the real
+     * heart-beats from this game server.
+     * 
+     * @return true, if another master server is sending heart-beat broadcasts
+     *         for this game server
+     *         false, if this master server is receiving the real heart-beats of
+     *         the game server
+     */
+
     public boolean isBroadcastedServer();
+
+    /**
+     * Gets the name of the game server, if no name of the game server has been
+     * received yet, it returns null
+     * 
+     * @return The server name if one has been received yet.
+     *         null, if none has been received yet
+     */
 
     public String getServerName();
 
